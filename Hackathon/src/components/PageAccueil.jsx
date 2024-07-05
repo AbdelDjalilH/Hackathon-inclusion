1eda7b98896fbd551f5445450c86cf3762b52ebb
BENSFIA <mobensfia@gmail.com>
2024-07-05 09:55:49 +0200

    creation branch final_branch et saisie code Page Accuiel

--- Hackathon/src/components/PageAccueil.jsx
+++ Hackathon/src/components/PageAccueil.jsx
@@ -5,6 +5,11 @@ export default function PageAccueil() {
       <h1>Bienvenue sur Allinclusion</h1>;
       <p> L'inclusion de nos talents sans discrimination</p>
       <img src={all_disc_1} alt="all disc 1" />
+      <div className="btn">
+        <button>Homme Femme travail égale salaire égale</button>
+        <button>Pas de discrimination raciale pour travailler</button>
+        <button>Handicapé moteur même droits pour acceder à l’emploi</button>
+      </div>
     </>
   );
 }
