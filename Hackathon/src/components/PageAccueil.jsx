708aa9ae824f926863a9d7eea42c0782b26fe456
BENSFIA <mobensfia@gmail.com>
2024-07-05 09:14:47 +0200

    creation branch final_branch et saisie code Page Accuiel

---
+++ Hackathon/src/components/PageAccueil.jsx
@@ -0,0 +1,10 @@
+import all_disc_1 from "../assets/all_disc_1.png";
+export default function PageAccueil() {
+  return (
+    <>
+      <h1>Bienvenue sur Allinclusion</h1>;
+      <p> L'inclusion de nos talents sans discrimination</p>
+      <img src={all_disc_1} alt="all disc 1" />
+    </>
+  );
+}
--- Hackathon/src/main.jsx
+++ Hackathon/src/main.jsx
@@ -7,6 +7,7 @@ import Accueil from "./components/Accueil";
 import Offre from "./components/Offre.jsx";
 import OffresDuMoment from "./components/OffresDuMoment.jsx";
 import ContactUs from "./components/ContactUs.jsx";
+import PageAccueil from "./components/PageAccueil.jsx";
 
 const router = createBrowserRouter([
   {
@@ -28,6 +29,10 @@ const router = createBrowserRouter([
       {
         path: "/nousContacter",
         element: <ContactUs />,
+      },
+      {
+        path: "/Accueil Allinclusion",
+        element: <PageAccueil />,
       },
     ],
   },
